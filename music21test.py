ddea3221f26ed361e3eeaff5019699ebf88e9ac6
fjonass <jsabini@pdx.edu>
2022-02-28 16:00:24 -0800

    show notes

--- music21test.py
+++ music21test.py
@@ -1,8 +1,18 @@
 from music21 import *
 
 import music21
-parsed = music21.converter.parse('test123.mid')
+parsed = music21.converter.parse('twinkleweird.mid')
 for elem in parsed.flat.notes:
 
-    print(elem.name)
+
+    if(hasattr(elem,"name")):
+        print(elem.name + str(elem.octave))
+    else:
+
+        for i in range(len(elem)):
+            print(elem[i].name + str(elem[i].octave),end=' ')
+        print("")
+
+
+
     #print(elem.quarterLength)
