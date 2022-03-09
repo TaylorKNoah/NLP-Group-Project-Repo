718dfdb60b97255633188871c0db979127335ea9
fjonass <jsabini@pdx.edu>
2022-03-09 14:58:19 -0800

    update with bass checking

--- midi_parser.py
+++ midi_parser.py
@@ -7,6 +7,8 @@ note_list = []
 def get_note_list(midifile):
 
     parsed = music21.converter.parse(midifile)
+    mid_note, mid_note_value, mid_note_octave = get_left_right_seperation(midifile)
+    print(mid_note, mid_note_value, mid_note_octave)
 
     for elem in parsed.flat.notes:
     
@@ -25,8 +27,12 @@ def get_note_list(midifile):
                 if elem[i].octave > int(highest_note[idx]):
                     highest_note = f'{elem[i].name}{str(elem[i].octave)}'
                     note = highest_note
-            
-        note_list.append(note)
+
+        if note_value(note[0]) > mid_note_value and int(note[len(note)-1]) >= mid_note_octave:
+            note_list.append(note)
+        elif note_value(note[0]) < mid_note_value and int(note[len(note)-1]) >= mid_note_octave:
+            note_list.append(note)
+
     return note_list
 
 def get_left_right_seperation(midifile):
@@ -69,16 +75,32 @@ def get_left_right_seperation(midifile):
     the_octave = round(note_value_octave/note_total)
 
     if(the_note == 3):
-        return "C"+str(the_octave)
+        return "C"+str(the_octave),the_note,the_octave
     elif(the_note == 4):
-        return "D" + str(the_octave)
+        return "D" + str(the_octave),the_note,the_octave
     elif (the_note == 5):
-        return "E" + str(the_octave)
+        return "E" + str(the_octave),the_note,the_octave
     elif (the_note == 6):
-        return "F" + str(the_octave)
+        return "F" + str(the_octave),the_note,the_octave
     elif (the_note == 7):
-        return "G" + str(the_octave)
+        return "G" + str(the_octave),the_note,the_octave
     elif (the_note == 8):
-        return "A" + str(the_octave)
+        return "A" + str(the_octave),the_note,the_octave
     elif (the_note == 9):
-        return "B" + str(the_octave)
+        return "B" + str(the_octave),the_note,the_octave
+
+def note_value(note):
+    if (note[0] == "C"):
+        return 3
+    elif (note[0] == "D"):
+        return 4
+    elif (note[0] == "E"):
+        return 5
+    elif (note[0] == "F"):
+        return 6
+    elif (note[0] == "G"):
+        return 7
+    elif (note[0] == "A"):
+        return 8
+    elif (note[0] == "B"):
+        return 9
